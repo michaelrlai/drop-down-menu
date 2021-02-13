b2f82c680edf601ae3d75188fd8604e7e32fecf7
Michael Lai <mrlai7782@gmail.com>
2021-02-13 03:21:07 -0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const renderDropdownMenus = function () { // Applies classes to each element of the menu
+const renderDropdownMenus = function () {
+    // Applies classes to each element of the menu
     const dropdowns = document.querySelectorAll(".dropdown-menu");
     for (i = 0; i < dropdowns.length; i++) {
         dropdowns[i]
             .querySelector("button")
             .classList.add("dropdown-open-button");
         dropdowns[i].querySelector("ul").classList.add("dropdown-list");
-        dropdowns[i].querySelector("ul").style.display = "none";
+        dropdowns[i].querySelector("ul").style.display = "none"; // Menu initally set to NOT be visible
         const listButtons = dropdowns[i].querySelectorAll("li");
         for (j = 0; j < listButtons.length; j++) {
             listButtons[j].classList.add("dropdown-list-button");
@@ -14,19 +15,17 @@ const renderDropdownMenus = function () { // Applies classes to each element of 
 };
 
 const clearDropdownMenus = function () {
+    // Clears all open dropdown menus
     const dropdownLists = document.querySelectorAll(".dropdown-list");
     for (k = 0; k < dropdownLists.length; k++) {
         dropdownLists[k].classList.remove("show");
-        dropdownLists[k].style.display = "none";
+        dropdownLists[k].style.display = "none"; // Sets menu to NOT be visible
     }
 };
 
 const dropdown = (function () {
     renderDropdownMenus();
     document.addEventListener("mousedown", function (e) {
-        console.log(e.target);
-        console.log(e.target.parentNode);
-        const dropdownLists = document.querySelectorAll(".dropdown-list");
         if (
             e.target.matches(".dropdown-open-button") &&
             !e.target.parentNode.querySelector("ul").matches(".show")
